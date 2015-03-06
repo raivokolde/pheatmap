f07e1ef72082237ca1b4e0ec884fea8ddee0476e
Lorena <lorena.pantano@gmail.com>
2015-03-06 12:13:41 -0500

    accept new ward.D ward.D2 options

--- R/pheatmap.r
+++ R/pheatmap.r
@@ -382,8 +382,8 @@ scale_colours = function(mat, col = rainbow(10), breaks = NA){
 }
 
 cluster_mat = function(mat, distance, method){
-    if(!(method %in% c("ward", "single", "complete", "average", "mcquitty", "median", "centroid"))){
-        stop("clustering method has to one form the list: 'ward', 'single', 'complete', 'average', 'mcquitty', 'median' or 'centroid'.")
+    if(!(method %in% c("ward", "ward.D", "ward.D2", "single", "complete", "average", "mcquitty", "median", "centroid"))){
+        stop("clustering method has to one form the list: 'ward', 'ward.D', 'ward.D2', 'single', 'complete', 'average', 'mcquitty', 'median' or 'centroid'.")
     }
     if(!(distance[1] %in% c("correlation", "euclidean", "maximum", "manhattan", "canberra", "binary", "minkowski")) & class(distance) != "dist"){
         stop("distance has to be a dissimilarity structure as produced by dist or one measure  form the list: 'correlation', 'euclidean', 'maximum', 'manhattan', 'canberra', 'binary', 'minkowski'")
