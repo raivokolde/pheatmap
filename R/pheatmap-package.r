bd1286b7f15f2399220b5a800907f38a628f2ff1
raivokolde <rkolde@gmail.com>
2015-03-09 17:59:20 -0400

    Trying to comply to CRAN

--- R/pheatmap-package.r
+++ R/pheatmap-package.r
@@ -1,10 +1,6 @@
 #' Pretty heatmaps
 #' 
-#' The package for drawing pretty heatmaps in R. The ordinary heatmap function in R has 
-#' several drawbacks when it comes to producing publication quality heatmaps. It is hard 
-#' to produce pictures with consistent text, cell and overall sizes. The function 
-#' pheatmap tries to alleviate the problems by offering more fine grained control over 
-#' heatmap dimensions and appearance.
+#' A grid based implementation of heatmaps that offers more control over heatmap dimensions and appearance.
 #' 
 #' @name pheatmap-package
 #' @docType package
